c27e21f640cc4cd95a4eabb7e4f83d2231260916
Ryan McKenzie <ryanjm88@gmail.com>
2018-08-06 19:56:11 -0400

    AJAX call update

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ $("#search").on("click", function () {
     console.log(query);
 
     $.ajax({
-        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=kitties&type=video&key=AIzaSyDVsUtmRBT_SXAPJCGmS3JFxLHMWJ0ZDbA",
+        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + query + "&type=video&key=AIzaSyDVsUtmRBT_SXAPJCGmS3JFxLHMWJ0ZDbA",
         method: "GET"
     }).then(function(response)  {
         console.log(response);
-        console.log(response.items);
+        console.log(response.items[0].id.videoId);
+        console.log(response.items[1].id.videoId);
 
-        $("#youtubeVid").append(response.items[0]);
+        $("#youtubeVid").append(`
+            <iframe id="player" type="text/html" width="300" height="300"
+            src="http://www.youtube.com/embed/${response.items[0].id.videoId}"
+            frameborder="0"></iframe>
+        `);
     });
 });
